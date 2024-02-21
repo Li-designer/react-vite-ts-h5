49b97529f43a4679b8bf8f5306300a53e8d3b75e
lijing <51325lj@sina.com>
2024-02-21 14:22:37 +0800

    fix:新增类型

--- src/context/LoadingContext.tsx
+++ src/context/LoadingContext.tsx
@@ -1,13 +1,16 @@
-import React, { createContext, useContext, useState } from "react"
+import React, { ReactNode, createContext, useContext, useState } from "react"
 
-const LoadingContext = createContext<{
-  loading?: boolean
-  setLoading?: Function
-}>({})
+export interface LoadingContextType {
+  loading: boolean
+  showLoading: (value: boolean) => void
+  hideLoading: (value: boolean) => void
+}
+
+const LoadingContext = createContext<LoadingContextType | null>(null)
 
 export const useLoading = () => useContext(LoadingContext)
 
-export const LoadingProvider = ({ children }) => {
+export const LoadingProvider = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState(false)
   const showLoading = () => {
     setLoading(true)
